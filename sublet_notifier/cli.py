729d9dfca176e494350a6098397df01a6b8e4250
Matthew Maclean <mmaclean@palantir.com>
2014-11-25 21:53:44 -0500

    Runs indefinitely

--- sublet_notifier/cli.py
+++ sublet_notifier/cli.py
@@ -1,10 +1,15 @@
 from sublet_notifier.commands.wloo_listings_scraper import WlooListingsScraper
 from sublet_notifier.email import send_email
+from time import sleep
 
 
 def main():
     scraper = WlooListingsScraper()
-    urls = scraper.get_sublets()
 
-    if len(urls) > 0:
-        return send_email("%s new sublets" % len(urls), "\n".join(urls))
+    while (1):
+        urls = scraper.get_sublets()
+
+        if len(urls) > 0:
+            send_email("%s new sublets" % len(urls), "\n".join(urls))
+
+        sleep(60 * 20)
